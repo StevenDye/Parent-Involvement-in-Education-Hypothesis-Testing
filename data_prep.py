2af0f6cc5973b5c7fb16c9dda4df765d0e8f63f9
Steven Dye <steven.p.dye@gmail.com>
2019-11-20 17:06:34 -0500

    Linted data_prep.py file at 17:06 on 20/11/19

--- data_prep.py
+++ data_prep.py
@@ -30,7 +30,7 @@ pi_rea_hm_feats = ['FHCHECKX', 'FHHELP', 'FORESPON']
 pi_freq_hm_feats = ['FODINNERX']
 # Combine all the features into a single list
 feats = perf_feats.copy()
-for feat in [pi_pro_schl_feats, pi_rea_schl_feats, pi_freq_schl_feats, \
+for feat in [pi_pro_schl_feats, pi_rea_schl_feats, pi_freq_schl_feats,
              pi_pro_hm_feats, pi_rea_hm_feats, pi_freq_hm_feats]:
     feats.extend(feat)
 # Append full sample weights
@@ -41,6 +41,8 @@ for c in df_cp.columns:
     df_cp = df_cp.drop(df_cp.loc[df_cp[c] < 0].index)
     if c == 'SEGRADES':
         df_cp = df_cp.drop(df_cp.loc[df_cp[c] == 5].index)
+
+
 # This function recodes binary parental involvement features so that
 # 1 shows less involvement and 2 shows more
 def feature_cleaning(features):
@@ -49,12 +51,14 @@ def feature_cleaning(features):
         i2 = df_cp.loc[df_cp[feat] == 2].index
         df_cp.loc[i1, feat] = 2
         df_cp.loc[i2, feat] = 1
+
+
 # Remove invalid entries from these features
-for feat in [pi_pro_schl_feats, pi_rea_schl_feats, \
+for feat in [pi_pro_schl_feats, pi_rea_schl_feats,
              pi_pro_hm_feats, pi_rea_hm_feats]:
     feature_cleaning(feat)
 # Create composite indices for proactive parental involvement
 df_cp['pi_pro_schl_feats_comp'] = df_cp[pi_pro_schl_feats].sum(axis=1)
 df_cp['pi_pro_hm_feats_comp'] = df_cp[pi_pro_hm_feats].sum(axis=1)
 pi_df = df_cp.copy()
-pi_df.to_csv('~/FIS-Projects/Module-3/FIS-Mod3-Project/data/pi_df.csv', sep = ',')
+pi_df.to_csv('~/FIS-Projects/Module-3/FIS-Mod3-Project/data/pi_df.csv', sep=',')
